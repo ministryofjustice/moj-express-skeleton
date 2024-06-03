50f931fe3aeda7350bb7bb15af3bd0cc692f60e7
psweeting <patrick.sweeting@digital.justice.gov.uk>
2024-06-03 09:13:29 +0100

    Refactor ES5 JS to ES6. Adding examples and set up for helmet and express-session

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ MOJ Express JS Skeleton to be used to help frontend projects.
 `DEBUG=moj-express-skeleton:* npm start`
 
 Then, load http://localhost:3000/ in your browser to access the app.
+
+### Apps Security
+
+- [helmet.js](https://helmetjs.github.io/)
+- [express-session](https://www.npmjs.com/package/express-session)
--- app.js
+++ app.js
@@ -1,35 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import helmet from 'helmet';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var helmet = require('helmet');
-var app = express();
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+const app = express();
+
 // Helmet can help protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'", "example.com"],
+      },
+    },
+  })
+);
+// Reducing fingerprinting
+app.disable('x-powered-by')
+
+// set up cookie security 
+app.set('trust proxy', 1); // trust first proxy
+app.use(session({
+  secret: 's3Cur3',
+  name: 'sessionId'
+}))
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(path.dirname(new URL(import.meta.url).pathname), 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(path.dirname(new URL(import.meta.url).pathname), 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
+// custom 404 and status example
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,4 +65,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
