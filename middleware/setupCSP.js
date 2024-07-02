b545eaf348e1605f0de30f62a32c78f20b926bd5
Patrick Sweeting <patrick.sweeting@digital.justice.gov.uk>
2024-07-02 09:11:05 +0100

    Generating nonce code for helmets CSP (#9)

---
+++ middleware/setupCSP.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+
+/**
+ * Middleware function to set up a Content Security Policy (CSP) nonce for each request.
+ *
+ * @param {Object} app - The Express application instance.
+ */
+function setupCSP(app) {
+    /**
+     * Middleware to generate a CSP nonce and add it to the response locals.
+     *
+     * @param {Object} req - The Express request object.
+     * @param {Object} res - The Express response object.
+     * @param {Function} next - The next middleware function in the stack.
+     */
+    function nonceMiddleware(req, res, next) {
+        res.locals.cspNonce = crypto.randomBytes(16).toString('hex');
+        next();
+    }
+
+    app.use(nonceMiddleware);
+}
+
+module.exports = setupCSP;
