c17d0ec415411026ccc7e35c605c567d5ee54916
psweeting <patrick.sweeting@digital.justice.gov.uk>
2024-07-09 15:38:20 +0100

    SQLite3 initalise and router example removed and added to README.md

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', (req, res, next) => {
   res.render('main/index', { title: 'Express' });
 });
 
-router.get('/users', (req, res, next) => {
-  req.db.all("SELECT * FROM users", (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({ users: rows });
-  });
-});
-
 module.exports = router;
