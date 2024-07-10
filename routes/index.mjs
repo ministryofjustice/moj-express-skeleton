761a4185c6908e5f7e5a9b88054655b69bd0a7d2
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-07-10 16:59:23 +0100

    - make axios make an API call to retrieve some fake users

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,9 +1,24 @@
 import express from 'express';
+import axios from 'axios';
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('main/index', { title: 'Express' });
+});
+
+// Make an API call with Axios 
+// GET users from external API
+router.get('/users', async (req, res, next) => {
+  try {
+    // json data of fake users
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching users');
+  }
 });
 
 export default router;
