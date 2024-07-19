af9e58c1f92bbd4ebb9ee7ed553f27eec5239653
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-07-19 15:55:15 +0100

    - remove example from `index.mjs`

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,4 +1,3 @@
-// routes/index.mjs
 import express from 'express';
 
 const router = express.Router();
@@ -6,18 +5,6 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('main/index', { title: 'Express' });
-});
-
-// Make an API call with `Axios` and `middleware-axios`
-// GET users from external API
-router.get('/users', async (req, res, next) => {
-  try {
-      // Use the Axios instance attached to the request object
-      const response = await req.axiosMiddleware.get('https://jsonplaceholder.typicode.com/users');
-      res.json(response.data);
-  } catch (error) {
-      next(error);
-  }
 });
 
 export default router;
