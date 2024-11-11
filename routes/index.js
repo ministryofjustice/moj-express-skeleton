79addf34046ea19486d35a58dd0846b9794d0376
psweeting <patrick.sweeting@digital.justice.gov.uk>
2024-11-11 08:29:25 +0000

    Removing get started examples from main skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { check, validationResult } from 'express-validator';
-import { csrfValidate } from '../middleware/csrfMiddleWare.js';
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  console.log(res.locals.nonce);
-  res.render('main/index', {errors: {}, data: {} });
-});
-
-// This is a basic example of express validation and should not be viewed as a development standard.
-router.post('/',csrfValidate, [
-  check('email').isEmail().withMessage('Invalid email').normalizeEmail(),
-  check('fullName').notEmpty().withMessage('Name is required').trim().escape(),
-  check('subscribe', 'Please select an option').notEmpty()
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.render('main/index', {
-      errors: errors.mapped(),
-      data: req.body
-    });
-  }
-  // on success
-  res.render('main/index', {
-    data: {success: true}
-  });
+  res.render('main/index');
 });
 
 export default router;
